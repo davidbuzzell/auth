f73da273b5d34bfa994d3ed2bb1e6d4463c2fdf8
David Buzzell <david.buzzell@kellogg.northwestern.edu>
2026-02-26 02:12:06 +0000

    managing logins

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,17 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "balls"
+# puts secret
+
 # 2. prepare encrypted string for testing
+salt = BCrypt::Password.create(secret)
+# puts salt
+
 # 3. test secret against prepared encrypted string
+test_string = "ballsy"
+if BCrypt::Password.new(salt) == test_string
+  puts "Hell yeah!"
+else
+  puts "Nah..."
+end
